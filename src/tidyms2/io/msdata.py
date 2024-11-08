6a9fdef7325be8eb5716aa2e95d36d40c3bcda17
Gabriel Riquelme <griquelme.chm@gmail.com>
2024-11-08 09:32:10 -0300

    feat(io): add method to temporarily override MSData config

--- src/tidyms2/io/msdata.py
+++ src/tidyms2/io/msdata.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import pathlib
 from collections import OrderedDict
+from contextlib import contextmanager
 from typing import Generator, Self
 
 from typing_extensions import Callable
@@ -18,10 +19,12 @@ class MSData:
 
     Data is read from disk in a lazy manner and cached in memory.
 
-    :param path: raw data file path.
+    :param src: raw data source. It may be the path to a raw data file or a sample model.
+        If the latter is provided, the path to the data source is fetch from the
+        :py:attr:`tidyms2.core.models.Sample.path` field.
     :param reader: the Reader to read raw data. If ``None``, the reader is inferred using the file extension.
         If an string is provided, a reader with the provided name is fetched from the reader registry.
-    :param ms_data_mode: the mode in which the data is stored.
+    :param mode: the mode in which the data is stored.
     :param centroider: a function that takes a spectrum in profile mode and converts it to centroid mode. Only
         used if `ms_data_mode` is set to profile mode.
     :param cache: int, default=-1
@@ -36,9 +39,9 @@ class MSData:
 
     def __init__(
         self,
-        path: pathlib.Path,
+        src: pathlib.Path | Sample,
         reader: type[Reader] | str | None = None,
-        ms_data_mode: MSDataMode = MSDataMode.CENTROID,
+        mode: MSDataMode = MSDataMode.CENTROID,
         centroider: Callable[[MSSpectrum], MSSpectrum] | None = None,
         cache: int = -1,
         ms_level: int = 1,
@@ -46,21 +49,26 @@ class MSData:
         end_time: float | None = None,
         **kwargs,
     ):
+        if isinstance(src, pathlib.Path):
+            id_ = src.name
+            sample = Sample(id=id_, path=src, ms_level=ms_level, start_time=start_time, end_time=end_time)
+        self.sample = sample
+
         if reader is None:
-            reader = reader_registry.get(path.suffix[1:])
+            reader = reader_registry.get(self.sample.path.suffix[1:])
         elif isinstance(reader, str):
             reader = reader_registry.get(reader)
 
-        self.ms_level = ms_level
-        self.start_time = start_time
-        self.end_time = end_time
-
-        self._centroider = centroider
-        self._reader = reader(path, **kwargs)
-        self._ms_data_mode = ms_data_mode
+        self.centroider = centroider
+        self._reader = reader(self.sample.path, **kwargs)
+        self.mode = mode
         self._cache = MSDataCache(max_size=cache)
         self._n_spectra: int | None = None
         self._n_chromatogram: int | None = None
+
+    def get_sample(self) -> Sample:
+        """Retrieve the sample associated with the data."""
+        return self.sample
 
     def get_n_chromatograms(self) -> int:
         """Retrieve the total number of chromatograms stored in the source."""
@@ -89,9 +97,9 @@ class MSData:
             spectrum = self._cache.get(index)
         else:
             spectrum = self._reader.get_spectrum(index)
-            if self._ms_data_mode == MSDataMode.PROFILE and self._centroider is not None:
-                spectrum = self._centroider(spectrum)
-            spectrum.centroid = self._ms_data_mode == MSDataMode.CENTROID
+            if self.mode == MSDataMode.PROFILE and self.centroider is not None:
+                spectrum = self.centroider(spectrum)
+            spectrum.centroid = self.mode == MSDataMode.CENTROID
             self._cache.add(spectrum)
         return spectrum
 
@@ -99,9 +107,34 @@ class MSData:
         """Iterate over all spectra in the data."""
         for k in range(self.get_n_spectra()):
             sp = self.get_spectrum(k)
-            if (self.ms_level == sp.ms_level) and (self.start_time <= sp.time):
-                if (self.end_time is None) or (self.end_time > sp.time):
+            if (self.sample.ms_level == sp.ms_level) and (self.sample.start_time <= sp.time):
+                if (self.sample.end_time is None) or (self.sample.end_time > sp.time):
                     yield sp
+
+    @contextmanager
+    def using_tmp_config(
+        self, ms_level: int | None = None, start_time: float | None = None, end_time: float | None = None
+    ):
+        """Context manager that temporarily modifies MS level and scans time range.
+
+        :param ms_level: temporary value for the MS level. If set to ``None`` the original value is not modified.
+        :param start_time: temporary value for the start time. If set to ``None`` the original value is not modified.
+        :param end_time: temporary value for the end time. If set to ``None`` the original value is not modified.
+
+        """
+        try:
+            sample = self.get_sample()
+            original_ms_level = sample.ms_level
+            original_start_time = sample.start_time
+            original_end_time = sample.end_time
+            sample.ms_level = sample.ms_level if ms_level is None else ms_level
+            sample.start_time = sample.start_time if start_time is None else start_time
+            sample.end_time = sample.end_time if end_time is None else end_time
+            yield
+        finally:
+            sample.ms_level = original_ms_level
+            sample.start_time = original_start_time
+            sample.end_time = original_end_time
 
     @classmethod
     def from_sample(cls, sample: Sample, cache: int = -1) -> Self:
