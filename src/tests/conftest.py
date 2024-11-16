20c3a4e023d580455580e426ec929d9f61497645
Gabriel Riquelme <griquelme.chm@gmail.com>
2024-11-16 15:10:38 -0300

    test: move conftest to test directory root

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -9,7 +9,7 @@ from tidyms2.lcms import simulation
 
 @pytest.fixture(scope="session")
 def data_dir():
-    tmp_data_path = pathlib.Path(__file__).parent.parent / "data"
+    tmp_data_path = pathlib.Path(__file__).parent / "data"
     if not tmp_data_path.exists():
         tmp_data_path.mkdir(exist_ok=True)
         gitignore = tmp_data_path / ".gitignore"
@@ -33,7 +33,7 @@ def lcms_sample_factory():
     config = simulation.SimulatedLCMSDataConfiguration(min_signal_intensity=1.0, n_scans=40, amp_noise=0.0)
     formula_list = ["[C54H104O6]+", "[C27H40O2]+", "[C24H26O12]+"]
     rt_list = [10.0, 20.0, 30.0]
-    int_list = [100.0, 200.0, 300.0]
+    int_list = [1000.0, 2000.0, 3000.0]
     adduct_list = list()
     for rt, spint, formula in zip(rt_list, int_list, formula_list):
         adduct = simulation.SimulatedLCMSAdductSpec(
