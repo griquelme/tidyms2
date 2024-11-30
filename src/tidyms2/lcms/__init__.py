94369844ca0eb8006bfe01cb3b006633159ce562
Gabriel Riquelme <griquelme.chm@gmail.com>
2024-11-30 13:42:01 -0300

    feat(lcms): add LC-MS assay utilities

--- src/tidyms2/lcms/__init__.py
+++ src/tidyms2/lcms/__init__.py
@@ -1,6 +1,7 @@
 """LC-MS utilities."""
 
+from .assay import create_lcms_assay
 from .models import MZTrace, Peak
 from .operators import LCTraceBaselineEstimator, LCTraceExtractor, PeakExtractor
 
-__all__ = ["LCTraceBaselineEstimator", "LCTraceExtractor", "MZTrace", "Peak", "PeakExtractor"]
+__all__ = ["create_lcms_assay", "LCTraceBaselineEstimator", "LCTraceExtractor", "MZTrace", "Peak", "PeakExtractor"]
