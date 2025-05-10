fd4ca5bbf22e6cf62777bf9948f4d65c2ae56a84
Gabriel Riquelme <griquelme.chm@gmail.com>
2025-05-10 14:34:07 -0300

    feat: add option to remove empty ROIs after sample processing

--- src/tidyms2/assay/executors.py
+++ src/tidyms2/assay/executors.py
@@ -9,7 +9,7 @@ import pydantic
 from ..core.exceptions import SampleProcessorError
 from ..core.models import FeatureType, RoiType
 from ..core.operators.pipeline import Pipeline
-from ..core.storage import AssayStorage, SampleStorage
+from ..core.storage import AssayStorage, SampleStorage, delete_empty_rois
 
 logger = getLogger(__name__)
 
@@ -27,8 +27,11 @@ class SampleProcessor(Protocol, Generic[RoiType, FeatureType]):
         ...
 
 
-class SequentialSampleProcessor(Generic[RoiType, FeatureType]):
+class SequentialSampleProcessor(pydantic.BaseModel, Generic[RoiType, FeatureType]):
     """Execute a sample pipeline."""
+
+    remove_empty_roi: bool = True
+    """Flag to delete ROIs that do not contain any features."""
 
     def execute(
         self,
@@ -41,11 +44,18 @@ class SequentialSampleProcessor(Generic[RoiType, FeatureType]):
         for k, sample in enumerate(samples, start=1):
             logger.info(f"Processing sample `{sample.get_sample().id}` ({k}/{n_samples}).")
             pipe.apply(sample)
+
+            if self.remove_empty_roi:
+                delete_empty_rois(sample)
+
             storage.add_sample_data(sample)
 
 
 class ParallelSampleProcessor(pydantic.BaseModel, Generic[RoiType, FeatureType]):
     """Execute a sample pipeline."""
+
+    remove_empty_roi: bool = True
+    """Flag to delete ROIs that do not contain any features."""
 
     max_workers: pydantic.PositiveInt = 2
     """The maximum number of process spawned simultaneously to process samples."""
@@ -63,6 +73,8 @@ class ParallelSampleProcessor(pydantic.BaseModel, Generic[RoiType, FeatureType])
             for k, future in enumerate(concurrent.futures.as_completed(futures), start=1):
                 data = future.result()
                 logger.info(f"Processed sample `{data.get_sample().id}` ({k}/{n_samples}).")
+                if self.remove_empty_roi:
+                    delete_empty_rois(data)
                 storage.add_sample_data(data)
 
 
@@ -72,6 +84,7 @@ def _sample_executor_worker(
     """Apply pipeline to a sample data instance."""
     try:
         pipeline.apply(data)
+        delete_empty_rois(data)
     except Exception as e:
         raise SampleProcessorError(f"Failed to process sample {data.get_sample().id}") from e
     return data
